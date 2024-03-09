6e3852e3c948a162dc07957b700d301bb72944f0
samitkumarpatel <samitkumarpatel@gmail.com>
2024-03-09 22:44:25 +0100

    oidc added , so that PKACE flow can continue

--- src/main/java/net/efullstack/ecomauthorisationserver/EcomAuthorisationServerApplication.java
+++ src/main/java/net/efullstack/ecomauthorisationserver/EcomAuthorisationServerApplication.java
@@ -10,14 +10,18 @@ import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 
 import java.util.List;
 
@@ -50,9 +54,25 @@ public class EcomAuthorisationServerApplication {
 	@Bean
 	@Order(1)
 	@SneakyThrows
-	//https://docs.spring.io/spring-security/reference/servlet/authorization/authorize-http-requests.html
+	//https://docs.spring.io/spring-authorization-server/reference/getting-started.html
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) {
 		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+
+		http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+					.oidc(Customizer.withDefaults());	// Enable OpenID Connect 1.0
+
+		// Redirect to the login page when not authenticated from the
+		// authorization endpoint
+		http.exceptionHandling((exceptions) -> exceptions
+				.defaultAuthenticationEntryPointFor(
+						new LoginUrlAuthenticationEntryPoint("/login"),
+						new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+				)
+		)
+		// Accept access tokens for User Info and/or Client Registration
+		.oauth2ResourceServer((resourceServer) -> resourceServer
+				.jwt(Customizer.withDefaults()));
+		http.cors(Customizer.withDefaults());
 		return http.build();
 	}
 
@@ -70,6 +90,7 @@ public class EcomAuthorisationServerApplication {
 						.ignoringRequestMatchers("/register")
 				)
 				.formLogin(Customizer.withDefaults())
+				.cors(Customizer.withDefaults())
 				/*.formLogin(f -> f.loginPage("/login").permitAll())
 				.logout((logout) -> logout
 						.logoutSuccessUrl("/logout")
